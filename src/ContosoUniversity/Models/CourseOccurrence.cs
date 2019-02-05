d98883ac5b37f21359366d726f82790eecafe90b
Erik Lallemand <erik.lallemand@gmail.com>
2019-02-05 12:57:44 +0100

    changement model occurrence

---
+++ src/ContosoUniversity/Models/CourseOccurrence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class CourseOccurrence
+    {
+        public int OccurrenceID { get; set; }
+        public int CourseID { get; set; }
+        public string Day { get; set; }
+        [Range(0, 23)]
+        public int Hour { get; set; }
+        [Range(0, 59)]
+        public int Minute { get; set; }
+        public int Duration { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
